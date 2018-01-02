2330dba965272b04af3a6f2a435198b2d4236da4
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2018-01-02 17:46:09 -0200

    testes de integração iniciais

---
+++ test/test_service_desk.js
@@ -0,0 +1,26 @@
+const assert = require("assert");
+const { ServiceDesk } = require("../src/classes/ServiceDesk");
+const { username, password } = require("../src/config/secrets");
+
+
+
+describe('Funcionalidades de ServiceDesk (browser visível)', () => {
+  let desk;
+  before(() => {
+    desk = new ServiceDesk();
+  });
+
+  after(() => {
+    desk.destroy();
+  });
+
+  it('deve criar um sessão de ServiceDesk com browser visível', (done) => {
+    assert(desk instanceof ServiceDesk, 'Não é uma instância válida');
+    done();
+  });
+
+  it('deve logar corretamente usando um usuário e senhas corretos', async function() {
+    await desk.logIn(username, password);
+    assert(desk.userName === username, 'Login gravado na sessão não bate com o fornecido');
+  });
+});
